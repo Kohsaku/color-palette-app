8552a2f66e8952d4909cc8bca033dbf25d27b974
Kohsaku <kosakuhayashi@gmail.com>
2022-05-07 20:41:38 +0900

    hslをrgb16進数に変換する関数設定。クリックしても表示されない不具合あり

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -35,24 +35,32 @@ const useStyles = makeStyles((theme) => ({
 interface ITEM {
   children: React.ReactNode;
   sx?: SxProps;
-  background: void;
+  background: string;
   fontColor: any;
 }
+
+type CHANGE_TO_16 = {
+  (arr: number[]): string;
+};
+
+type HSL_TO_RGB = {
+  (hsl: string): string;
+};
 
 type FONT_COLOR_CHANGE = {
   (hex: string): string;
 };
 
 type ON_CLICK_COPY = {
-  (hex: string): string;
+  (hex: string): void;
 };
 
-const Item = ({ sx, children, background, fontColor }: ITEM) => (
+const Item = ({ children, background, fontColor }: ITEM) => (
   <Paper
     square={true}
     sx={{
       color: fontColor,
-      backgroundColor: background,
+      bgcolor: background,
       paddingTop: (theme: Theme) => theme.spacing(12),
       height: (theme: Theme) => theme.spacing(12),
       width: (theme: Theme) => theme.spacing(24),
@@ -140,66 +148,81 @@ const Home: React.FC = () => {
     });
     setDisplayPalette(true);
     setPalette({ ...palette, colors: randomColor });
+    console.log(palette.colors);
+    console.log(displayPalette);
   };
 
-  const changeTo16 = (arr: number[]) => {
-    const RGB_MAX = 255;
-    const HUE_MAX = 360;
-    const SATURATION_MAX = 100;
-    const LIGHTNESS_MAX = 100;
-    let r, g, b, max, min;
+  const changeTo16: CHANGE_TO_16 = (arr) => {
+    let result: any;
+    let hue = arr[0];
+    let saturation = arr[1];
+    let lightness = arr[2];
 
-    let h = arr[0] % HUE_MAX;
-    let s = arr[1] / SATURATION_MAX;
-    let l = arr[2] / LIGHTNESS_MAX;
+    if (
+      (hue || hue === 0) &&
+      hue <= 360 &&
+      (saturation || saturation === 0) &&
+      saturation <= 100 &&
+      (lightness || lightness === 0) &&
+      lightness <= 100
+    ) {
+      var red = 0;
+      var green = 0;
+      var blue = 0;
+      var q = 0;
+      var p = 0;
+      var hueToRgb;
 
-    if (l < 0.5) {
-      max = l + l * s;
-      min = l - l * s;
-    } else {
-      max = l + (1 - l) * s;
-      min = l - (1 - l) * s;
+      hue = Number(hue) / 360;
+      saturation = Number(saturation) / 100;
+      lightness = Number(lightness) / 100;
+
+      if (saturation === 0) {
+        red = lightness;
+        green = lightness;
+        blue = lightness;
+      } else {
+        hueToRgb = function (p: number, q: number, t: number) {
+          if (t < 0) t += 1;
+          if (t > 1) t -= 1;
+
+          if (t < 1 / 6) {
+            p += (q - p) * 6 * t;
+          } else if (t < 1 / 2) {
+            p = q;
+          } else if (t < 2 / 3) {
+            p += (q - p) * (2 / 3 - t) * 6;
+          }
+
+          return p;
+        };
+
+        if (lightness < 0.5) {
+          q = lightness * (1 + saturation);
+        } else {
+          q = lightness + saturation - lightness * saturation;
+        }
+        p = 2 * lightness - q;
+
+        red = hueToRgb(p, q, hue + 1 / 3);
+        green = hueToRgb(p, q, hue);
+        blue = hueToRgb(p, q, hue - 1 / 3);
+      }
+
+      result = `#${Math.round(red * 255).toString(16)}${Math.round(
+        green * 255
+      ).toString(16)}${Math.round(blue * 255).toString(16)}`;
     }
 
-    const hp = HUE_MAX / 6;
-    const q = h / hp;
-    if (q <= 1) {
-      r = max;
-      g = (h / hp) * (max - min) + min;
-      b = min;
-    } else if (q <= 2) {
-      r = ((hp * 2 - h) / hp) * (max - min) + min;
-      g = max;
-      b = min;
-    } else if (q <= 3) {
-      r = min;
-      g = max;
-      b = ((h - hp * 2) / hp) * (max - min) + min;
-    } else if (q <= 4) {
-      r = min;
-      g = ((hp * 4 - h) / hp) * (max - min) + min;
-      b = max;
-    } else if (q <= 5) {
-      r = ((h - hp * 4) / hp) * (max - min) + min;
-      g = min;
-      b = max;
-    } else {
-      r = max;
-      g = min;
-      b = ((HUE_MAX - h) / hp) * (max - min) + min;
-    }
-
-    let red = r * RGB_MAX;
-    let green = g * RGB_MAX;
-    let blue = b * RGB_MAX;
-    return `${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+    return result;
   };
 
-  const hslToRgb = (hsl: string) => {
+  const hslToRgb: HSL_TO_RGB = (hsl) => {
     let sliced = hsl.slice(4, -1);
     let splited = sliced.split(",");
     let toInt = splited.map((str) => parseInt(str));
-    changeTo16(toInt);
+    console.log(changeTo16(toInt));
+    return changeTo16(toInt);
   };
 
   const fontColorChange: FONT_COLOR_CHANGE = (hex) => {
